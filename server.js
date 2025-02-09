2214806cef4a2de5700b3fff2458ae4e9a5611e2
Siddhuu-7 <siddhardhautla19@gmail.com>
2025-02-09 13:58:41 +0530

    fixed

--- server.js
+++ server.js
@@ -1,38 +1,23 @@
-const express = require('express');
-const cors = require('cors');
-const userRoutes = require('./routes/user.routes');
-const userRequest = require('./routes/request.route');
-const searchEngine = require('./routes/search.route');
-const SeekAndSeeking = require('./routes/SeekAndSeekers.route');
-const Goals = require('./routes/Goals.route');
-const Message = require('./routes/Message.route');
-const socketHandler = require('./routes/socket.route'); 
-
-require('dotenv').config();
-
-const app = express();
-const http = require('http');
-const server = http.createServer(app);
-
-socketHandler(server);
-
-app.use(express.json());
-const PORT = process.env.PORT || 5000;
-
+const express=require('express');
+const userRoutes=require('./routes/user.routes')
+const userResquest=require('./routes/request.route')
+const cors=require('cors')
+require('dotenv').config()
+const app=express();
+const bcrypt=require('bcrypt')
+const searchEngine=require('./routes/search.route')
+const SeekAndSeking=require('./routes/SeekAndSeekers.route')
+const Goals=require('./routes/Goals.route')
+app.use(express.json())
+const PORT=process.env.PORT
 app.use(cors({
-    origin: '*', // Allow all origins
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Ensure all necessary methods are allowed
-    allowedHeaders: ['Content-Type', 'Authorization'], // Allow necessary headers
-  }));
-  
-
-app.use('/user', userRoutes);
-app.use('', userRequest);
-app.use('/', Goals);
-app.use('', searchEngine);
-app.use('', SeekAndSeeking);
-app.use('', Message);
-
-server.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+    origin:"*"
+}))
+app.use('/user',userRoutes)
+app.use('',userResquest)
+app.use('/',Goals)
+app.use('',searchEngine)
+app.use('',SeekAndSeking)
+app.listen(PORT,()=>{
+    console.log(`http://localhost:${PORT}`);
+})
